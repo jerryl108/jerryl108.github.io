0b7f940f22c98cc6a11350ceb3c741f99d378862
Jerry Linder <glz67@mst.edu>
2019-03-26 08:12:07 -0500

    try a sticky nav bar

---
+++ stickynav.js
@@ -0,0 +1,20 @@
+function setup_sticky_nav()
+{
+    var nav_element = document.getElementsByTagName("nav")[0];
+
+    var trigger_height = nav_element.offsetTop;
+
+    function update_nav()
+    {
+        if (window.pageYOffset >= trigger_height)
+        {
+            nav_element.classList.add("sticky");
+        }
+        else
+        {
+            nav_element.classList.remove("sticky");
+        }
+    }
+
+    document.onscroll(update_nav)
+}
