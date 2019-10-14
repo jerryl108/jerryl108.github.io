5ba1e1826fcb94291bcbcfd0f2e90fa0af6d209d
root <root@DESKTOP-VG63O60.localdomain>
2019-10-13 19:50:21 -0500

    changed style to make more sense, made color changes

--- stickynav.js
+++ stickynav.js
@@ -1,6 +1,6 @@
 function setup_sticky_nav()
 {
-    var nav_element = document.getElementsByTagName("aside")[0];
+    var nav_element = document.getElementById("nav-container")[0];
 
     var trigger_height = nav_element.offsetTop;
 
